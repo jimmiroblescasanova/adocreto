5d133fc1aec1d3d25695648e86b28a2c44465a6a
Jimmi Robles <jimmirobles@icloud.com>
2024-12-18 12:59:39 -0500

    Enhances category resource with validation and relationships
    Adds required validation and length constraints to the name field in the category form.
    
    Introduces new relationships for materials and products in the Category model, allowing for better data management.
    
    Improves user experience by ensuring fields are validated and extending model functionality.

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -20,68 +20,76 @@ class CategoryResource extends Resource
 
     protected static bool $isScopedToTenant = false;
 
+    protected static ?string $modelLabel = 'categoría';
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                ->label('Nombre'),
+        ->schema([
+            Forms\Components\TextInput::make('name')
+            ->label('Nombre')
+            ->required()
+            ->minLength(5)
+            ->maxLength(255),
 
-                Forms\Components\ColorPicker::make('color')
-                ->label('Color')
-                ->hex(),
+            Forms\Components\ColorPicker::make('color')
+            ->label('Color')
+            ->hex(),
 
-                Forms\Components\Textarea::make('description')
-                ->label('Descripción')
-                ->columnSpanFull(),
-            ]);
+            Forms\Components\Textarea::make('description')
+            ->label('Descripción')
+            ->columnSpanFull(),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('name')
-                ->label('Nombre')
-                ->searchable()
-                ->sortable()
-                ->grow(),
+        ->defaultSort('name', 'asc')
+        ->deferLoading()
+        ->columns([
+            Tables\Columns\TextColumn::make('name')
+            ->label('Nombre')
+            ->searchable()
+            ->sortable()
+            ->grow(),
 
-                Tables\Columns\ColorColumn::make('color')
-                ->label('Color'),
+            Tables\Columns\ColorColumn::make('color')
+            ->label('Color')
+            ->sortable(),
 
-                Tables\Columns\TextColumn::make('created_at')
-                ->label('Fecha de creación')
-                ->date()
-                ->alignEnd()
-                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('created_at')
+            ->label('Fecha de creación')
+            ->date()
+            ->alignEnd()
+            ->toggleable(isToggledHiddenByDefault: true),
 
-                Tables\Columns\TextColumn::make('updated_at')
-                ->label('Últ. actualización')
-                ->since()
-                ->alignEnd()
-                ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make()
-                ->mutateFormDataUsing(function (array $data): array {
-                    $data['name'] = Str::title($data['name']);
-             
-                    return $data;
-                }),
-                
-                Tables\Actions\DeleteAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+            Tables\Columns\TextColumn::make('updated_at')
+            ->label('Últ. actualización')
+            ->since()
+            ->alignEnd()
+            ->toggleable(isToggledHiddenByDefault: true),
+        ])
+        ->filters([
+            //
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make()
+            ->mutateFormDataUsing(function (array $data): array {
+                $data['name'] = Str::title($data['name']);
+            
+                return $data;
+            }),
+            
+            Tables\Actions\DeleteAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]),
+        ]);
     }
 
     public static function getPages(): array
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    public function materials(): HasMany
+    {
+        return $this->hasMany(Material::class); 
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class); 
+    }
 }
