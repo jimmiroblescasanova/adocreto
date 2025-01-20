4a2c7e29b086a0bf8c80e4306fd42a4cdc70549a
Jimmi Robles <jimmirobles@icloud.com>
2025-01-20 15:53:37 -0500

    Refactor DocumentType enum to improve clarity and consistency
    - Replaced 'Transfer' with 'Production' and updated its label to 'Orden de producción'.
    - Renamed 'Ticket' to 'Sale' and adjusted its label to 'Venta'.
    - These changes enhance the semantic meaning of document types in the application.

--- app/Enums/DocumentType.php
+++ app/Enums/DocumentType.php
@@ -8,9 +8,9 @@ enum DocumentType: int implements HasLabel
 {
     case InventoryIn = 1;
     case InventoryOut = 2;
-    case Transfer = 3;
+    case Production = 3;
     case Order = 4;
-    case Ticket = 5;
+    case Sale = 5;
     case Invoice = 6;
 
     public function getLabel(): string
@@ -18,9 +18,9 @@ enum DocumentType: int implements HasLabel
         return match ($this) {
             self::InventoryIn => 'Entrada',
             self::InventoryOut => 'Salida',
-            self::Transfer => 'Traspaso',
+            self::Production => 'Orden de producción',
             self::Order => 'Cotizacion',
-            self::Ticket => 'Ticket',
+            self::Sale => 'Venta',
             self::Invoice => 'Factura',
         };
     }
