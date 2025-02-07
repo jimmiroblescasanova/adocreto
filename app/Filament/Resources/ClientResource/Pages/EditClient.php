11bd19e8d628d2eb30102343abc64f3e09282652
Jimmi Robles <jimmirobles@icloud.com>
2025-02-07 16:58:02 -0500

    Add RedirectsAfterSave trait to EditClient page
    - Integrated RedirectsAfterSave trait in EditClient page
    - Enhances client editing workflow with consistent post-save redirection behavior

--- app/Filament/Resources/ClientResource/Pages/EditClient.php
+++ app/Filament/Resources/ClientResource/Pages/EditClient.php
@@ -4,12 +4,13 @@ namespace App\Filament\Resources\ClientResource\Pages;
 
 use Filament\Actions;
 use App\Traits\EditActionsOnTop;
+use App\Traits\RedirectsAfterSave;
 use Filament\Resources\Pages\EditRecord;
 use App\Filament\Resources\ClientResource;
 
 class EditClient extends EditRecord
 {
-    use EditActionsOnTop;
+    use EditActionsOnTop, RedirectsAfterSave;
     
     protected static string $resource = ClientResource::class;
 }
---
+++ app/Traits/RedirectsAfterSave.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App\Traits;
+
+trait RedirectsAfterSave
+{
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+}
