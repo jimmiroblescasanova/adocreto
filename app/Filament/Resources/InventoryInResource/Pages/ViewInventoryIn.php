280ccf1bce4e4b1877ea605b16075acc858257c6
Jimmi Robles <jimmirobles@icloud.com>
2025-03-17 12:48:34 -0500

    Add cancel action to ViewInventoryIn; implement document cancellation logic and error handling

--- app/Filament/Resources/InventoryInResource/Pages/ViewInventoryIn.php
+++ app/Filament/Resources/InventoryInResource/Pages/ViewInventoryIn.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Resources\InventoryInResource\Pages;
 
+use App\Enums\DocumentStatus;
+use App\Enums\InventoryOperation;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 use App\Filament\Components\Actions\BackButton;
 use App\Filament\Resources\InventoryInResource;
+use Filament\Notifications\Notification;
+use Filament\Support\Enums\IconPosition;
 
 class ViewInventoryIn extends ViewRecord
 {
@@ -17,6 +21,38 @@ class ViewInventoryIn extends ViewRecord
             Actions\EditAction::make(),
 
             BackButton::make(),
+
+            Actions\ActionGroup::make([
+                Actions\Action::make('cancel')
+                ->label('Cancelar')
+                ->icon('heroicon-o-x-circle')
+                ->action(fn () => $this->cancelDocument())
+                ->requiresConfirmation(),
+            ])
+            ->label('Acciones')
+            ->icon('heroicon-o-ellipsis-horizontal-circle')
+            ->iconPosition(IconPosition::After)
+            ->button()
+            ->color('gray'),
         ];
+    }
+
+    private function cancelDocument(): void
+    {
+        try {
+            $this->record->items()->update([
+                'operation' => InventoryOperation::NoEffect,
+            ]);
+
+            $this->record->update([
+                'status' => DocumentStatus::Cancelled,
+            ]);
+        } catch (\Throwable $th) {
+            Notification::make()
+            ->title('Error al cancelar')
+            ->body($th->getMessage())
+            ->danger()
+            ->send();
+        }
     }
 }
